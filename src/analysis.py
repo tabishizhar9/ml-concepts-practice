1b42bfc6231441a8c9be9c04ba539f3a97985689
tabish.izhar@stud.uni-bamberg.de <tabish.izhar@stud.uni-bamberg.de>
2026-03-07 08:48:25 +0100

    chore: practice handling missing values using mean and median

--- src/analysis.py
+++ src/analysis.py
@@ -2,5 +2,10 @@ import pandas as pd
 
 df =  pd.read_csv("data/students.csv")
 # print(df.head(8))
-# print(df.info())cl
-print(df.isna())
+# print(df.info())
+print(df.isna().sum())
+score_cols = ["reading", "writing"]
+df[score_cols] = df[score_cols].apply(pd.to_numeric, errors="coerce")
+df[score_cols] = df[score_cols].fillna(df[score_cols].mean())
+print(df.isna().sum())
+print(df.head(8))

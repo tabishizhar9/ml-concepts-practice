c72a79b08428dfcb3a530fcb101ed98c0e1cd1bf
tabish.izhar@stud.uni-bamberg.de <tabish.izhar@stud.uni-bamberg.de>
2026-03-08 02:02:09 +0100

    chore: add average_score column and label data as Pass/Fail

--- src/analysis.py
+++ src/analysis.py
@@ -1,11 +1,27 @@
 import pandas as pd
+import numpy as np
 
 df =  pd.read_csv("data/students.csv")
+score_cols = ["reading", "writing"]
+
+
 # print(df.head(8))
 # print(df.info())
-print(df.isna().sum())
-score_cols = ["reading", "writing"]
-df[score_cols] = df[score_cols].apply(pd.to_numeric, errors="coerce")
-df[score_cols] = df[score_cols].fillna(df[score_cols].mean())
-print(df.isna().sum())
+
+#########       practice handling missing values using mean and median
+# print(df.isna().sum())
+# score_cols = ["reading", "writing"]
+# df[score_cols] = df[score_cols].apply(pd.to_numeric, errors="coerce")
+# df[score_cols] = df[score_cols].fillna(df[score_cols].mean())
+# print(df.isna().sum())
+# print(df.head(8))
+
+#########       added a feature/ column average_score
+df["avg_score"] = df[score_cols].mean(axis=1)
+print(df[["name", "avg_score"]].head())
 print(df.head(8))
+
+########        label the data Pass / Fail
+df["pass"] = np.where(df["avg_score"] >= 60, "Yes", "No")
+print(df[["name", "avg_score", "pass"]].head(8))
+
